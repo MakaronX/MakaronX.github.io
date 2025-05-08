7e9f1eda753ed2a1da6cbbc3763851f55f557c6a
Oleh Khmilovskyi <119733982+MakaronX@users.noreply.github.com>
2025-05-08 20:54:44 +0200

    fix test

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 import { slider } from './components/slider.js';
 slider();
 
-import { modal } from './components/modal.js';
-modal();
-
-// import { language } from './components/language.js';
-// language();
+import { language } from './components/language.js';
+language();
